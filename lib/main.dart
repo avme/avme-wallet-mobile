295a83aa5ecc23f8a0eda6dc849ab3ada846ded7
Gabriel Vilarinho <contato@gabrielvilarinho.com>
2021-04-24 16:05:10 -0300

    Created basic operation of the wallet as JSON, read file and write generated file/wallet.

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
 import 'dart:io';
 import 'package:web3dart/web3dart.dart';
@@ -10,8 +11,9 @@ import 'package:avme_wallet/Screen/helper.dart';
 import 'package:after_layout/after_layout.dart';
 import 'package:bip39/bip39.dart' as bip39;
 
-String hex = "";
 var random = Null;
+
+final String password = "abacate";
 void main() {
   runApp(AvmeWallet());
 }
@@ -21,10 +23,13 @@ class AvmeWallet extends StatelessWidget with Helpers {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Login(),
+      // home: Login(),
+      home: Scaffold(
+        appBar: AppBar(title: Text("AVME Wallet")),
+        body: LoginTwo(),
+      )
     );
   }
-
 }
 
 class Login extends StatefulWidget
@@ -40,96 +45,170 @@ class Login extends StatefulWidget
   }
 }
 
+class LoginTwo extends StatelessWidget with Helpers
+{
+  @override
+  Widget build(BuildContext context) {
+      return ListView(
+        children: [
+          Card(
+            child: ListTile(
+              title: Text("1 - Load Current Wallet"),
+              trailing: ElevatedButton(
+                onPressed: () {
+                  btnLoadWallet(context);
+                },
+                child: Text("Try me!"),
+              ),
+            )
+          ),
+          Card(
+              child: ListTile(
+                title: Text("2 - New Wallet"),
+                trailing: ElevatedButton(
+                  onPressed: () {
+                    btnMakeWallet(context);
+                  },
+                  child: Text("Try me!"),
+                ),
+              )
+          )
+        ],
+      );
+  }
+  btnLoadWallet(BuildContext context) async
+  {
+
+    snack("Trying to load...", context);
+    WalletManager wm = new WalletManager(hash:"futa");
+    File fileP = await wm._localFile;
+    String content = new File(fileP.path).readAsStringSync();
+    Wallet wallet = Wallet.fromJson(content, password);
+    // snack(content, context);
+    //Check the credentials
+    Credentials accessGranted = wallet.privateKey;
+    snack(accessGranted.toString(), context);
+  }
+  btnMakeWallet(BuildContext context) async
+  {
+    String hex = await WalletManager().generateSeed();
+    // WalletManager wm = new WalletManager(hash:hex);
+    WalletManager wm = new WalletManager(hash:"futa");
+    var _rng = new Random.secure();
+    // Credentials _random = EthPrivateKey.createRandom(_rng);
+    Credentials credentFromHex = EthPrivateKey.fromHex(hex);
+    Wallet wallet = Wallet.createNew(credentFromHex,password, _rng);
+    String json = wallet.toJson();
+    // snack(wallet.toJson(), context);
+
+    // SAVING THE WALLET
+
+
+    // UNCOMMENT TO SHOW THE PATH
+    // File pathString = await wm._localFile;
+    // snack(pathString.path, context);
+
+    File path = await wm.write(json);
+    snack("Saved to: "+path.path, context);
+  }
+  // @override
+  // void afterFirstLayout(BuildContext context) {
+  //   onLoad(context);
+  // }
+}
+
 class LoginState extends State<Login> with AfterLayoutMixin <Login>, Helpers
 {
 
   @override
   Widget build(BuildContext context) {
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("AVME Wallet"),
-      ),
-      body:
-        Column(
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                btn1(context);
-              },
-              child: Text("btn1"),
-            ),
+    return Row(children: [
+              // Row(children: [
+              //
+              //     Column(
+              //       children: [
+              //         Image.network(
+              //           'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg',
+              //           height: 150,
+              //           width: 150,
+              //         ),
+              //         Image.asset('assets/images/supreme_f.png'
+              //         ),
+              //       ],
+              //     ),
+              // ],),
+              Row(children:[
+                Column(
+                  children: [
+                    // ElevatedButton(
+                    //   onPressed: () {
+                    //     btn1(context);
+                    //   },
+                    //   child: Text("btn1"),
+                    // ),
+                    //
+                    // ElevatedButton(
+                    //   onPressed: () {
+                    //
+                    //   },
+                    //   child: Text("btn2"),
+                    // ),
+                    // ElevatedButton(
+                    //   onPressed: () {
+                    //     hasPermission(context);
+                    //   },
+                    //   child: Text("hasPermission"),
+                    // ),
+                    ElevatedButton(
+                      onPressed: () {
+                        btnMakeWallet(context);
+                      },
+                      child: Text("Make Wallet"),
+                    )
 
-            ElevatedButton(
-              onPressed: () {
-                btn2(context);
-              },
-              child: Text("btn2"),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                btnMakeWallet(context);
-              },
-              child: Text("Make Wallet"),
-            )
+                  ],
+                ),
+              ])
+        ]);
 
-          ],
-        ),
-    );
   }
   onLoad(BuildContext context)
   {
-      debugPrint('ANAL');
+
   }
   btn1(context)
   {
       snack("botao apertado", context);
   }
-  btn2(context)
+
+  btnMakeWallet(BuildContext context) async
   {
-      String preMnemonic = "cross burst million health capital category salt float velvet clerk version always";
-      // UNCOMMENT THE NEXT LINE TO GENERATE ANOTHER
-      // var mnemonic = bip39.generateMnemonic();
-      // GENERATIONG HEX
-
-      var _hex = bip39.mnemonicToSeedHex(preMnemonic);
-      hex = _hex;
-      snack(_hex, context);
-      // PRINTING DATA USING SNACKBAR
-      // debugPrint(mnemonic);
-      // snack(mnemonic.toString(), context);
-
-
-  }
-  btnMakeWallet(BuildContext context)
-  async {
-      //ALLOCATING WALLET DATA
-
-      // String content = new File("wallet.json").readAsStringSync();
-      // Wallet wallet = Wallet.fromJson(content, "test");
-
-      //DESCOMENTAR
+      String hex = await WalletManager().generateSeed();
+      // WalletManager wm = new WalletManager(hash:hex);
+      WalletManager wm = new WalletManager(hash:"futa");
       var _rng = new Random.secure();
       // Credentials _random = EthPrivateKey.createRandom(_rng);
       Credentials credentFromHex = EthPrivateKey.fromHex(hex);
       Wallet wallet = Wallet.createNew(credentFromHex,"abacate", _rng);
+      String json = wallet.toJson();
+      // snack(wallet.toJson(), context);
 
-      snack(wallet.toJson(), context);
+      // SAVING THE WALLET
 
-      // CREATING FILE ON THE CREATED WALLET
-      // todo: refactor this code
 
-      // Async because the app will request access to the device...
+      // UNCOMMENT TO SHOW THE PATH
+      // File pathString = await wm._localFile;
+      // snack(pathString.path, context);
 
-      // String path = await WalletManager()._localPath;
-      // snack(path, context);
+      File path = await wm.write(json);
+      snack("Saved to: "+path.path, context);
   }
   @override
   void afterFirstLayout(BuildContext context) {
     onLoad(context);
   }
 }
-
 // CREATING FILE ON THE CREATED WALLET
 // todo: refactor this code
 
@@ -137,49 +216,95 @@ class LoginState extends State<Login> with AfterLayoutMixin <Login>, Helpers
 
 class WalletManager
 {
-    //Our constructor
-    final String hash;
-    WalletManager({this.hash});
+  //Our constructor
+  final String hash;
+  WalletManager({this.hash = ""});
 
-    String ext = ".json";
-    String filename = "AVME/wallet-";
+  String ext = ".json";
+  String folder = "AVME-Wallet/";
+  String filename = "wallet-";
 
 
-    // GET THE DEFAULT PATH
-    // Android: /data/user/0/com.avme.avme_wallet/app_flutter
-    Future<String> get _localPath async
+  // GET THE DEFAULT PATH
+  // Android: /data/user/0/com.avme.avme_wallet/app_flutter
+  Future<String> get _localPath async
+  {
+    final directory = await getApplicationDocumentsDirectory();
+
+    return directory.path;
+  }
+  // // SETTING THE FILE PATH
+  // Future<File> get _localFile async
+  // {
+  //   final path = await _localPath;
+  //   return File('$path/$filename$hash$ext');
+  // }
+
+  // SETTING THE FILE PATH
+  Future<File> get _localFile async
+  {
+    final path = await _localPath;
+    final bool exists = await checkPath("$path/$folder");
+    String fullPath;
+    if(exists)
     {
-        final directory = await getApplicationDocumentsDirectory();
+         fullPath = "$path/$folder$filename$hash$ext";
+    }
+    return File(fullPath);
+  }
 
-        return directory.path;
-    }
-    // SETTING THE FILE PATH
-    Future<File> get _localFile async
+  // WRITTING DATA
+  Future<File> write(String json) async
+  {
+    final file = await _localFile;
+    return file.writeAsString("$json");
+  }
+  // READING DATA
+  Future<String> read() async
+  {
+    try
     {
-        final path = await _localPath;
-        return File('$path/$filename$hash$ext');
-    }
-    // WRITTING DATA
-    Future<File> write(String json) async
-    {
-        final file = await _localFile;
-        return file.writeAsString("$json");
-    }
-    // READING DATA
-    Future<String> read() async
-    {
-        try
-        {
-            // Waits our path to resolve
-            final file = await _localFile;
-            // Read file
-            String contents = await file.readAsString();
+      // Waits our path to resolve
+      final file = await _localFile;
+      // Read file
+      String contents = await file.readAsString();
 
-            return contents;
-        }
-        catch(e)
-        {
-            debugPrint(e.toString());
-        }
+      return contents;
     }
+    catch(e)
+    {
+      debugPrint(e.toString());
+    }
+    return null;
+  }
+  // VALIDATE THE GIVEN PATH, OTHERWISE CREATES THE DIRECTORY
+  Future<bool> checkPath(path) async
+  {
+      bool exists = await Directory(path).exists();
+      if(exists.toString() == "false")
+      {
+          var directory = await Directory(path).create(recursive: true);
+          debugPrint("CREATING THE DIRECTORY: " + directory.path);
+          exists = true;
+      }
+      else
+      {
+          debugPrint("DIRECTORY ALREADY EXISTS!" + path);
+      }
+      return exists;
+  }
+
+  Future<String> generateSeed() async
+  {
+
+    String preMnemonic = "cross burst million health capital category salt float velvet clerk version always";
+
+    // UNCOMMENT THE NEXT LINE TO GENERATE ANOTHER
+    // var mnemonic = bip39.generateMnemonic();
+
+    // GENERATIONG HEX
+    return bip39.mnemonicToSeedHex(preMnemonic);
+    // debugPrint(mnemonic);
+  }
 }
+
