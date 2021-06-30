b2345460475897b3bd49cd75d3b205fdd0b6b38b
Gabriel Vilarinho <contato@gabrielvilarinho.com>
2021-06-30 17:39:12 -0300

    Finished chart implementation

--- lib/app/screens/widgets/balance/line_chart.dart
+++ lib/app/screens/widgets/balance/line_chart.dart
@@ -18,8 +18,13 @@ class _TokenChartState extends State<TokenChart> {
     const Color(0xff02d39a),
   ];
   bool showAvg;
+  List<FlSpot> chartSpots;
+  Map<dynamic, dynamic> daysAndPrice;
   @override
   Widget build(BuildContext context) {
+    daysAndPrice = organizeValue();
+    chartSpots = getChartPoints();
+
     return Stack(
       children: <Widget>[
        Card(
@@ -31,8 +36,8 @@ class _TokenChartState extends State<TokenChart> {
              crossAxisAlignment: CrossAxisAlignment.start,
              children: [
                Padding(
-                 padding: const EdgeInsets.only(top: 12, bottom: 15),
-                 child: Text("AVME Chart:",
+                 padding: const EdgeInsets.only(top: 12,),
+                 child: Text("Market Data:",
                      style: TextStyle(
                          fontWeight: FontWeight.bold, fontSize: 16)),
                ),
@@ -43,19 +48,97 @@ class _TokenChartState extends State<TokenChart> {
                      Expanded(
                        child: AspectRatio(
                          aspectRatio: 4/3,
-                         child: LineChart(
-                               // showAvg ? avgData() : mainData(),
-                               mainData(),
-                             ),
+                         child: Padding(
+                           padding: const EdgeInsets.only(right:18.0,left: 8),
+                           child: Stack(
+                             children:
+                             [
+                               LineChart(
+                                 lines(),
+                               ),
+                               LineChart(
+                                 mainData(),
+                               ),
+                             ]
                            ),
                          ),
-                     ]
+                       ),
+                     ),
+                   ]
                  ),
                )
              ],
            )
          ),
        ),
+      ],
+    );
+  }
+
+  LineChartData lines() {
+    const Color bgColors = Color(0xFF747474);
+    return LineChartData(
+      gridData: FlGridData(
+        show: true,
+        drawVerticalLine: true,
+        getDrawingHorizontalLine: (value) {
+          return FlLine(
+            color: bgColors,
+            strokeWidth: 1,
+          );
+        },
+        getDrawingVerticalLine: (value) {
+          return FlLine(
+            color: bgColors,
+            strokeWidth: 1,
+          );
+        },
+      ),
+      titlesData: FlTitlesData(
+        show: true,
+        bottomTitles: SideTitles(
+          showTitles: true,
+          getTextStyles: (value) =>
+          const TextStyle(color: Colors.transparent, fontWeight: FontWeight.bold, fontSize: 16),
+        ),
+        leftTitles: SideTitles(
+          showTitles: true,
+          getTextStyles: (value) => const TextStyle(
+            color: Colors.transparent,
+            fontWeight: FontWeight.bold,
+            fontSize: 15,
+          ),
+          getTitles: (value) =>  getHeight(value),
+          reservedSize: 28,
+          // margin: 8,
+        ),
+        topTitles: SideTitles(
+            showTitles: true,
+            getTextStyles: (value) => const TextStyle(
+              color:  Colors.transparent,
+              fontWeight: FontWeight.bold,
+              fontSize: 15,
+            ),
+            getTitles: (value) {
+              if(value == 7) return "Historical AVME Prices";
+              return null;
+            }
+        ),
+      ),
+      borderData:
+      FlBorderData(show: true, border: Border.all(color: const Color(0xff37434d), width: 1)),
+      minX: 0,
+      maxX: 15,
+      minY: 0,
+      maxY: 4,
+      lineBarsData: [
+        LineChartBarData(
+          isCurved: true,
+          barWidth: 5,
+          dotData: FlDotData(
+            show: false,
+          ),
+        ),
       ],
     );
   }
@@ -85,9 +168,9 @@ class _TokenChartState extends State<TokenChart> {
           showTitles: true,
           reservedSize: 22,
           getTextStyles: (value) =>
-          const TextStyle(color: Color(0xffffffff), fontWeight: FontWeight.bold, fontSize: 16),
+          const TextStyle(color: Color(0xffffffff), fontWeight: FontWeight.bold, fontSize: 14),
           getTitles: (value) => getDays(value),
-          margin: 8,
+          // margin: 8,
         ),
         leftTitles: SideTitles(
           showTitles: true,
@@ -98,7 +181,19 @@ class _TokenChartState extends State<TokenChart> {
           ),
           getTitles: (value) =>  getHeight(value),
           reservedSize: 28,
-          margin: 8,
+          // margin: 8,
+        ),
+        topTitles: SideTitles(
+            showTitles: true,
+            getTextStyles: (value) => const TextStyle(
+              color:  Color(0xffffffff),
+              fontWeight: FontWeight.bold,
+              fontSize: 15,
+            ),
+            getTitles: (value) {
+              if(value == 7) return "Historical AVME Prices";
+              return null;
+            }
         ),
       ),
       borderData:
@@ -106,18 +201,23 @@ class _TokenChartState extends State<TokenChart> {
       minX: 0,
       maxX: 15,
       minY: 0,
-      maxY: 6,
+      maxY: 2,
+      lineTouchData: LineTouchData(
+        touchTooltipData: LineTouchTooltipData(
+          getTooltipItems: (value)
+            {
+              return value.map((e) => LineTooltipItem(
+                  "${e.y} USD\n${daysAndPrice[e.x][1]}",
+                  const TextStyle(color: Color(0xff23b6e6), fontWeight: FontWeight.bold, fontSize: 16),
+                )
+              ).toList();
+            }
+        )
+      ),
       lineBarsData: [
         LineChartBarData(
-          spots: [
-            FlSpot(0, 1),
-            FlSpot(2.6, 2),
-            FlSpot(4.9, 5),
-            FlSpot(6.8, 3.1),
-            FlSpot(8, 4),
-            FlSpot(9.5, 3),
-            FlSpot(11, 4),
-          ],
+          spots: getChartPoints(),
+
           isCurved: true,
           colors: gradientColors,
           barWidth: 5,
@@ -139,11 +239,13 @@ class _TokenChartState extends State<TokenChart> {
     double highest = widget.appState.dashboard.getHighestValue()[1] * 1.15;
 
     switch (value.toInt()) {
+      case 0:
+        return "0.0\n\n";
+
       case 1:
-        return (highest/3).toStringAsPrecision(2);
-      case 3:
         return (highest/2).toStringAsPrecision(2);
-      case 5:
+
+      case 2:
         return highest.toStringAsPrecision(2);
     }
     return '';
@@ -153,10 +255,11 @@ class _TokenChartState extends State<TokenChart> {
   {
     int lastday =  widget.appState.dashboard.getHighestDay()[0];
     int secondsday10 = 864000;
-    DateTime dateTimeYesterday  = DateTime.fromMillisecondsSinceEpoch(lastday * 1000);
+    // DateTime dateTimeYesterday  = DateTime.fromMillisecondsSinceEpoch(lastday * 1000);
+    DateTime today  = DateTime.fromMillisecondsSinceEpoch((lastday + 86400) * 1000);
     // DateTime dateTime10  = DateTime.fromMillisecondsSinceEpoch((lastday - secondsday10) * 1000);
-    DateTime dateTime20 = DateTime.fromMillisecondsSinceEpoch((lastday - secondsday10 * 2 ) * 1000);
-    DateTime dateTime30 = DateTime.fromMillisecondsSinceEpoch((lastday - secondsday10 * 3) * 1000);
+    DateTime dateTime20 = DateTime.fromMillisecondsSinceEpoch((lastday - secondsday10 * 2) * 1000);
+    DateTime dateTime30 = DateTime.fromMillisecondsSinceEpoch(((lastday + 86400) - secondsday10 * 3) * 1000);
 
     switch (value.toInt()) {
       case 0:
@@ -164,12 +267,55 @@ class _TokenChartState extends State<TokenChart> {
       case 7:
         return DateFormat('MM/dd').format(dateTime20).toString();
       case 14:
-        return DateFormat('MM/dd').format(dateTimeYesterday).toString();
+        return DateFormat('MM/dd').format(today).toString();
     }
     return '';
   }
-  /*
-    DateTime date = DateTime.fromMicrosecondsSinceEpoch(card["unixDate"],isUtc: false);
-    DateFormat dateFormat = DateFormat('MM-dd-yyyy hh:mm:ss');
-   */
+
+  Map<dynamic, dynamic> organizeValue()
+  {
+    int totalDays = widget.appState.dashboard.tokenList.length + 1;
+    int _totalDays = totalDays;
+    Map ret = widget.appState.dashboard.tokenList.map((key, value) {
+      if(totalDays == _totalDays)
+      {
+        totalDays--;
+        double key = (_totalDays - 1)/2;
+        return MapEntry(key, [_totalDays+1, DateFormat('MM/dd').format(DateTime.now()).toString(), double.parse(double.parse(widget.appState.token.value).toStringAsPrecision(3))]);
+      }
+      else
+      {
+        totalDays--;
+        DateTime day = DateTime.fromMillisecondsSinceEpoch((key) * 1000);
+        return MapEntry(((totalDays-1)/2).toDouble(),[totalDays, DateFormat('MM/dd').format(day).toString(), double.parse(double.parse(value).toStringAsPrecision(3))]);
+      }
+    });
+    return ret;
+  }
+
+  List<FlSpot> getChartPoints()
+  {
+    /// FLSpot (dia,valor) FlSpot(0, 1)
+    List<FlSpot> valueSpots = [];
+    daysAndPrice.forEach((key, value) {
+      valueSpots.add(FlSpot(key,value[2]));
+    });
+
+    return valueSpots;
+  }
+  // List<FlSpot> getChartPoints()
+  // {
+  //   /// FLSpot (dia,valor) FlSpot(0, 1)
+  //   int order = 0;
+  //   int totalDays = widget.appState.dashboard.tokenList.length + 1;
+  //   List<FlSpot> valueSpots = [];
+  //   widget.appState.dashboard.tokenList.forEach((key, value) {
+  //     order++;
+  //     totalDays--;
+  //     DateTime day = DateTime.fromMillisecondsSinceEpoch((key + 86400) * 1000);
+  //     valueSpots.add(FlSpot(((totalDays-1) / 2).toDouble(),double.parse(double.parse(value).toStringAsPrecision(3))));
+  //     return MapEntry(order,{key: [totalDays, DateFormat('MM/dd').format(day).toString(), double.parse(value).toStringAsPrecision(3)]});
+  //   });
+  //   return valueSpots;
+  // }
 }
