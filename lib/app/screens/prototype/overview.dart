b3ca93da740e499de87db52f49a763ae63a48faf
Gabriel Vilarinho <contato@gabrielvilarinho.com>
2021-08-16 19:15:49 -0300

    Added fancy animation in \Overview"

--- lib/app/screens/prototype/overview.dart
+++ lib/app/screens/prototype/overview.dart
@@ -11,18 +11,7 @@ class Overview extends StatelessWidget {
         AppCard(
           child: Column(
             children: [
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
-                  gradient: LinearGradient(
-                    begin: Alignment.centerLeft,
-                    end: Alignment.centerRight,
-                    colors: <Color>[
-                      AppColors.purpleVariant2,
-                      AppColors.lightBlue
-                    ]
-                  )
-                ),
+              GradientContainer(
                 child: Padding(
                   padding: const EdgeInsets.all(16.0),
                   child: Row(
@@ -120,6 +109,74 @@ class Overview extends StatelessWidget {
           ),
         ),
       ],
+    );
+  }
+}
+
+class GradientContainer extends StatefulWidget {
+  final Widget child;
+
+  const GradientContainer({@required this.child});
+  @override
+  _GradientContainerState createState() => _GradientContainerState();
+}
+
+class _GradientContainerState extends State<GradientContainer>
+    with TickerProviderStateMixin {
+  ///Tween are "state" or states of the transition our explicit transition
+  ///will animate.
+  final DecorationTween decorationTween = DecorationTween(
+    begin: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        gradient: LinearGradient(
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+            colors: <Color>[
+              AppColors.purpleVariant2,
+              AppColors.lightBlue,
+            ]
+        )
+    ),
+    end: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        gradient: LinearGradient(
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+            colors: <Color>[
+              AppColors.lightBlue,
+              AppColors.purpleVariant2,
+            ]
+        )
+    ),
+  );
+
+  AnimationController _controller;
+
+  @override
+  void initState()
+  {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 30),
+    )..repeat(reverse: true);
+  }
+
+  @override
+  void dispose()
+  {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: DecoratedBoxTransition(
+        position: DecorationPosition.background,
+        decoration: decorationTween.animate(_controller),
+        child: Container(child: widget.child),
+      ),
     );
   }
 }
