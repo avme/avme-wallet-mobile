16fbd34e2699e6728753fbebf6e119d5bc0f2d69
Gabriel Vilarinho <contato@gabrielvilarinho.com>
2021-11-19 14:15:25 -0300

    Added new widgets from flutter.pub to assist with "Connection Service"

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:avme_wallet/app/controller/contacts.dart';
+import 'package:avme_wallet/app/controller/services/connection.dart';
 import 'package:avme_wallet/app/model/token_chart.dart';
 import 'package:avme_wallet/app/screens/widgets/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:avme_wallet/app/config/routes.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
 import 'package:avme_wallet/app/model/app.dart';
+import 'package:overlay_support/overlay_support.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/services.dart';
 import 'package:hive/hive.dart';
@@ -14,6 +16,10 @@ import 'app/controller/file_manager.dart';
 
 main() async{
   WidgetsFlutterBinding.ensureInitialized();
+
+  AppConnection appConnection = AppConnection.getInstance();
+  appConnection.initialize();
+
   await DotEnv.load();
   await Hive.initFlutter();
   Hive.registerAdapter(TokenChartAdapter());
@@ -40,10 +46,12 @@ class AvmeWalletApp extends StatelessWidget {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
       statusBarColor: Colors.transparent,
     ));
-    return MaterialApp(
-      theme: avmeTheme,
-      initialRoute: defaultRoute,
-      routes: routes);
+    return OverlaySupport.global(
+      child: MaterialApp(
+        theme: avmeTheme,
+        initialRoute: defaultRoute,
+        routes: routes),
+    );
   }
 }
 
