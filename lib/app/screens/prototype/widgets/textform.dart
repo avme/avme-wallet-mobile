6e54a95e540870ddae964f6325cf7a2d3119a286
Guilherme Ratib <cyan.tear1999@gmail.com>
2021-12-16 17:37:02 -0300

    Gabriel - Added QR Code icon fade on focus

--- lib/app/screens/prototype/widgets/textform.dart
+++ lib/app/screens/prototype/widgets/textform.dart
@@ -1,7 +1,8 @@
 import 'package:avme_wallet/app/screens/widgets/theme.dart';
 import 'package:flutter/material.dart';
 
-class AppTextFormField extends StatelessWidget {
+
+class AppTextFormField extends StatefulWidget {
 
   final Color cursorColor;
   final Function validator;
@@ -42,132 +43,162 @@ class AppTextFormField extends StatelessWidget {
     this.textAlign = TextAlign.left,
     this.hintText,
     this.contentPadding = const EdgeInsets.symmetric(
-      vertical: 16,
-      horizontal: 12
+        vertical: 16,
+        horizontal: 12
     ),
   }) : super(key: key);
+  
+  @override
+  _AppTextFormFieldState createState() => _AppTextFormFieldState();
+}
+
+class _AppTextFormFieldState extends State<AppTextFormField> with TickerProviderStateMixin {
+  bool hideFloatingIcon = false;
+  FocusNode myFocus;
+  AnimationController animation;
+  Animation<double> fade;
+  @override
+  void initState() {
+    myFocus = FocusNode();
+    animation = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 200),
+    );
+    fade = Tween<double>(begin: 1, end: 0).animate(animation);
+    myFocus.addListener(() {
+      if(hideFloatingIcon)
+        animation.reverse();
+      else
+        animation.forward();
+      setState(() {
+        hideFloatingIcon = !hideFloatingIcon;
+      });
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
+    myFocus = widget.focusNode ?? myFocus;
     Color cLabelStyle = AppColors.labelDefaultColor;
     FontWeight fLabelStyle = FontWeight.w900;
     OutlineInputBorder fieldBorder = OutlineInputBorder(
-      borderRadius: BorderRadius.circular(6.0),
+        borderRadius: BorderRadius.circular(6.0),
         borderSide: BorderSide(
-          width: 2
+            width: 2
         )
     );
 
-    EdgeInsets varContentPadding;
-    if(this.icon!=null)
-    {
-      varContentPadding = EdgeInsets.only(top:16, bottom: 16, left:12, right: 40);
-    } else
-    {
-      //previous default value
-      varContentPadding = this.contentPadding;
-    }
+    EdgeInsets varContentPadding = widget.contentPadding;
+    varContentPadding = EdgeInsets.only(top:16, bottom: 16, left:12, right: hideFloatingIcon ? 12 : 40);
 
-    if(this.focusNode != null)
-    {
-      cLabelStyle = this.focusNode.hasFocus ? AppColors.purple : AppColors.labelDefaultColor;
-      fLabelStyle = this.focusNode.hasFocus ? FontWeight.w900 : FontWeight.w500;
-    }
+    cLabelStyle = myFocus.hasFocus ? AppColors.purple : AppColors.labelDefaultColor;
+    fLabelStyle = myFocus.hasFocus ? FontWeight.w900 : FontWeight.w500;
 
     return Stack(
       children: [
         Column(
           children: [
             TextFormField(
-              enabled: this.enabled,
-              validator: this.validator,
-              controller: this.controller,
-              cursorColor: this.cursorColor,
-              obscureText: this.obscureText,
-              initialValue: this.initialValue,
-              textAlign: this.textAlign,
-              focusNode: this.focusNode,
-              keyboardType: this.keyboardType,
-              onChanged: this.onChanged,
-              decoration: InputDecoration(
-                isDense: true,
-                filled: true,
-                hintText: this.hintText,
-                fillColor: AppColors.darkBlue,
-                focusedErrorBorder: fieldBorder.copyWith(
+                enabled: widget.enabled,
+                validator: widget.validator,
+                controller: widget.controller,
+                cursorColor: widget.cursorColor,
+                obscureText: widget.obscureText,
+                initialValue: widget.initialValue,
+                textAlign: widget.textAlign,
+                focusNode: myFocus,
+                keyboardType: widget.keyboardType,
+                onChanged: widget.onChanged,
+                decoration: InputDecoration(
+                  isDense: true,
+                  filled: true,
+                  hintText: widget.hintText,
+                  fillColor: AppColors.darkBlue,
+                  focusedErrorBorder: fieldBorder.copyWith(
+                      borderSide: BorderSide(
+                        width: 2,
+                        color: Colors.red,
+                      )
+                  ),
+                  errorBorder: fieldBorder.copyWith(
+                      borderSide: BorderSide(
+                        width: 2,
+                        color: AppColors.labelDefaultColor,
+                      )
+                  ),
+                  labelText: widget.labelText,
+                  floatingLabelBehavior: widget.floatingLabelBehavior,
+                  contentPadding: varContentPadding,
+                  enabledBorder: fieldBorder.copyWith(
                     borderSide: BorderSide(
                       width: 2,
-                      color: Colors.red,
-                    )
-                ),
-                errorBorder: fieldBorder.copyWith(
+                      color: cLabelStyle,
+                    ),
+                  ),
+                  disabledBorder: OutlineInputBorder(
+                      borderRadius: fieldBorder.borderRadius,
+                      borderSide: BorderSide(
+                          width: 0,
+                          color: Colors.transparent
+                      )
+                  ),
+                  labelStyle: TextStyle(
+                      color: cLabelStyle,
+                      fontWeight: fLabelStyle,
+                      fontSize: 20
+                  ),
+                  focusedBorder: fieldBorder.copyWith(
                     borderSide: BorderSide(
-                      width: 2,
-                      color: AppColors.labelDefaultColor,
-                    )
-                ),
-                labelText: this.labelText,
-                floatingLabelBehavior: this.floatingLabelBehavior,
-                contentPadding: varContentPadding,
-                enabledBorder: fieldBorder.copyWith(
-                  borderSide: BorderSide(
-                    width: 2,
-                    color: cLabelStyle,
+                        width: 2,
+                        color: AppColors.purple
+                    ),
                   ),
-                ),
-                disabledBorder: OutlineInputBorder(
-                  borderRadius: fieldBorder.borderRadius,
-                  borderSide: BorderSide(
-                    width: 0,
-                    color: Colors.transparent
-                  )
-                ),
-                labelStyle: TextStyle(
-                    color: cLabelStyle,
-                    fontWeight: fLabelStyle,
-                    fontSize: 20
-                ),
-                focusedBorder: fieldBorder.copyWith(
-                  borderSide: BorderSide(
-                      width: 2,
-                      color: AppColors.purple
-                  ),
-                ),
-              )
+                )
             ),
           ],
         ),
-        this.icon != null
-        ? Positioned.fill(
+        widget.icon != null
+            ? Positioned.fill(
             child: Align(
                 alignment: Alignment.topRight,
-                child: GestureDetector(
-                  onTap: this.iconOnTap,
-                  child: Container(
-                    // color: Color.fromRGBO(255, 50, 50, 0.2),
-                    color: Colors.transparent,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        SizedBox(
-                          // width: 48,
-                          child: Padding(
-                            padding: const EdgeInsets.all(9),
-                            child: Container(
-                              color: AppColors.darkBlue,
-                              child: this.icon)
-                            ,
+                child: FadeTransition(
+                  opacity: fade,
+                  child: GestureDetector(
+                    onTap: widget.iconOnTap,
+                    child: Container(
+                      // color: Color.fromRGBO(255, 50, 50, 0.2),
+                      color: Colors.transparent,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          SizedBox(
+                            // width: 48,
+                            child: Padding(
+                              padding: const EdgeInsets.all(9),
+                              child: Container(
+                                  color: AppColors.darkBlue,
+                                  child: widget.icon)
+                              ,
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
                 )
             )
         )
-        : Container()
+            : Container()
       ],
     );
   }
+
+  @override
+  void dispose() {
+    animation.dispose();
+    super.dispose();
+  }
 }
+
