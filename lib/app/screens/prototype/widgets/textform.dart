74d1ba1b4e8898f1ee33367154e7931fcde62d09
Guilherme Ratib <cyan.tear1999@gmail.com>
2022-02-11 13:10:26 -0300

    Added a onFieldSubmitted parameter for input navigation

--- lib/app/screens/prototype/widgets/textform.dart
+++ lib/app/screens/prototype/widgets/textform.dart
@@ -3,9 +3,7 @@ import 'package:avme_wallet/app/screens/widgets/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-
 class AppTextFormField extends StatefulWidget {
-
   final Color cursorColor;
   final Function validator;
   final Function onChanged;
@@ -26,39 +24,40 @@ class AppTextFormField extends StatefulWidget {
   final TextAlign textAlign;
   final int maxLength;
   final TextInputAction textInputAction;
+  final Function onFieldSubmitted;
 
-  const AppTextFormField({
-    Key key,
-    this.cursorColor = AppColors.purple,
-    this.validator,
-    this.onChanged,
-    this.controller,
-    this.obscureText = false,
-    this.focusNode,
-    this.labelText,
-    this.enabled,
-    this.icon,
-    this.formKey,
-    this.iconOnTap,
-    this.floatingLabelBehavior = FloatingLabelBehavior.always,
-    this.initialValue,
-    this.keyboardType,
-    this.isDense = false,
-    this.textAlign = TextAlign.left,
-    this.hintText,
-    this.contentPadding = const EdgeInsets.symmetric(
-        vertical: 16,
-        horizontal: 12
-    ),
-    this.maxLength,
-    this.textInputAction
-  }) : super(key: key);
+  const AppTextFormField(
+      {Key key,
+      this.cursorColor = AppColors.purple,
+      this.validator,
+      this.onChanged,
+      this.controller,
+      this.obscureText = false,
+      this.focusNode,
+      this.labelText,
+      this.enabled,
+      this.icon,
+      this.formKey,
+      this.iconOnTap,
+      this.floatingLabelBehavior = FloatingLabelBehavior.always,
+      this.initialValue,
+      this.keyboardType,
+      this.isDense = false,
+      this.textAlign = TextAlign.left,
+      this.hintText,
+      this.onFieldSubmitted,
+      this.contentPadding =
+          const EdgeInsets.symmetric(vertical: 16, horizontal: 12),
+      this.maxLength,
+      this.textInputAction})
+      : super(key: key);
 
   @override
   _AppTextFormFieldState createState() => _AppTextFormFieldState();
 }
 
-class _AppTextFormFieldState extends State<AppTextFormField> with TickerProviderStateMixin {
+class _AppTextFormFieldState extends State<AppTextFormField>
+    with TickerProviderStateMixin {
   bool hideFloatingIcon = false;
   FocusNode myFocus;
   AnimationController animation;
@@ -74,7 +73,7 @@ class _AppTextFormFieldState extends State<AppTextFormField> with TickerProvider
     );
     fade = Tween<double>(begin: 1, end: 0).animate(animation);
     myFocus.addListener(() {
-      if(hideFloatingIcon)
+      if (hideFloatingIcon)
         animation.reverse();
       else
         animation.forward();
@@ -82,15 +81,14 @@ class _AppTextFormFieldState extends State<AppTextFormField> with TickerProvider
         hideFloatingIcon = !hideFloatingIcon;
       });
     });
-    if(widget.controller != null)
-    {
-      if(widget.maxLength != null)
+    if (widget.controller != null) {
+      if (widget.maxLength != null)
         widget.controller.addListener(() {
           setState(() {});
         });
     }
-
   }
+
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
@@ -99,134 +97,135 @@ class _AppTextFormFieldState extends State<AppTextFormField> with TickerProvider
     FontWeight fLabelStyle = FontWeight.w900;
     OutlineInputBorder fieldBorder = OutlineInputBorder(
         borderRadius: BorderRadius.circular(6.0),
-        borderSide: BorderSide(
-            width: 2
-        )
-    );
+        borderSide: BorderSide(width: 2));
 
     EdgeInsets contentPadding = widget.contentPadding;
-    contentPadding = EdgeInsets.only(top:16, bottom: 16, left:12, right: !hideFloatingIcon && widget.icon != null ? 40 : 12);
+    contentPadding = EdgeInsets.only(
+        top: 16,
+        bottom: 16,
+        left: 12,
+        right: !hideFloatingIcon && widget.icon != null ? 40 : 12);
     // contentPadding = EdgeInsets.only(top:16, bottom: 16, left:12, right: hideFloatingIcon ? 12 : 40);
 
     ///Testing if isDense is true, we set same as the default in
     ///"flutter: material/input_decorator.dart", since the padding ain't
     ///being set by the framework
-    contentPadding = widget.isDense ? EdgeInsets.fromLTRB(8, 8, 0, 8) : contentPadding;
+    contentPadding =
+        widget.isDense ? EdgeInsets.fromLTRB(8, 8, 0, 8) : contentPadding;
 
-    cLabelStyle = myFocus.hasFocus ? AppColors.purple : AppColors.labelDefaultColor;
+    cLabelStyle =
+        myFocus.hasFocus ? AppColors.purple : AppColors.labelDefaultColor;
     fLabelStyle = myFocus.hasFocus ? FontWeight.w900 : FontWeight.w500;
+
+    //Function added for adding custom parameters or passing focus
+    Function(String) onFieldSubmitted;
+    if (widget.onFieldSubmitted == null) {
+      onFieldSubmitted = (_) => FocusScope.of(context).unfocus();
+    } else {
+      onFieldSubmitted = widget.onFieldSubmitted;
+    }
 
     return Stack(
       children: [
         Column(
           children: [
             TextFormField(
-              textInputAction: widget.textInputAction,
-              maxLength: widget.maxLength,
-              maxLengthEnforcement: widget.maxLength != null
-                ? MaxLengthEnforcement.enforced
-                : MaxLengthEnforcement.none,
-              enabled: widget.enabled,
-              validator: widget.validator,
-              controller: widget.controller,
-              cursorColor: widget.cursorColor,
-              obscureText: widget.obscureText,
-              initialValue: widget.initialValue,
-              textAlign: widget.textAlign,
-              focusNode: myFocus,
-              keyboardType: widget.keyboardType,
-              onChanged: widget.onChanged,
-              decoration: InputDecoration(
-                isDense: widget.isDense,
-                filled: true,
-                hintText: widget.hintText,
-                fillColor: AppColors.darkBlue,
-                focusedErrorBorder: fieldBorder.copyWith(
-                  borderSide: BorderSide(
-                    width: 2,
-                    color: Colors.red,
-                  )
-                ),
-                errorBorder: fieldBorder.copyWith(
-                  borderSide: BorderSide(
-                    width: 2,
-                    color: AppColors.labelDefaultColor,
-                  )
-                ),
-                labelText: widget.labelText,
-                floatingLabelBehavior: widget.floatingLabelBehavior,
-                contentPadding: contentPadding,
-                enabledBorder: fieldBorder.copyWith(
-                  borderSide: BorderSide(
-                    width: 2,
-                    color: cLabelStyle,
-                  ),
-                ),
-                disabledBorder: OutlineInputBorder(
-                  borderRadius: fieldBorder.borderRadius,
-                  borderSide: BorderSide(
-                    width: 0,
-                    color: Colors.transparent
-                  )
-                ),
-                labelStyle: TextStyle(
-                  color: cLabelStyle,
-                  fontWeight: fLabelStyle,
-                  fontSize: SizeConfig.fontSize
-                ),
-                focusedBorder: fieldBorder.copyWith(
-                  borderSide: BorderSide(
-                    width: 2,
-                    color: AppColors.purple
-                  ),
-                ),
-                counter: widget.maxLength != null
-                  ? Align(
-                      alignment: Alignment.centerRight.add(Alignment(SizeConfig.blockSizeHorizontal / 100 * 1.5, 0)),
-                      child: Container(
-                        // color:Colors.red,
-                        child: Text("${widget.controller.text.length}/${widget.maxLength}",
-                          textDirection: TextDirection.rtl,
-                          style: AppTextStyles.span.copyWith(fontSize: SizeConfig.fontSize),
-                        )
+                textInputAction: widget.textInputAction,
+                maxLength: widget.maxLength,
+                maxLengthEnforcement: widget.maxLength != null
+                    ? MaxLengthEnforcement.enforced
+                    : MaxLengthEnforcement.none,
+                enabled: widget.enabled,
+                validator: widget.validator,
+                controller: widget.controller,
+                cursorColor: widget.cursorColor,
+                obscureText: widget.obscureText,
+                initialValue: widget.initialValue,
+                textAlign: widget.textAlign,
+                focusNode: myFocus,
+                keyboardType: widget.keyboardType,
+                onChanged: widget.onChanged,
+                onFieldSubmitted: onFieldSubmitted,
+                decoration: InputDecoration(
+                    isDense: widget.isDense,
+                    filled: true,
+                    hintText: widget.hintText,
+                    fillColor: AppColors.darkBlue,
+                    focusedErrorBorder: fieldBorder.copyWith(
+                        borderSide: BorderSide(
+                      width: 2,
+                      color: Colors.red,
+                    )),
+                    errorBorder: fieldBorder.copyWith(
+                        borderSide: BorderSide(
+                      width: 2,
+                      color: AppColors.labelDefaultColor,
+                    )),
+                    labelText: widget.labelText,
+                    floatingLabelBehavior: widget.floatingLabelBehavior,
+                    contentPadding: contentPadding,
+                    enabledBorder: fieldBorder.copyWith(
+                      borderSide: BorderSide(
+                        width: 2,
+                        color: cLabelStyle,
                       ),
-                    )
-                  : null
-              )
-            ),
+                    ),
+                    disabledBorder: OutlineInputBorder(
+                        borderRadius: fieldBorder.borderRadius,
+                        borderSide:
+                            BorderSide(width: 0, color: Colors.transparent)),
+                    labelStyle: TextStyle(
+                        color: cLabelStyle,
+                        fontWeight: fLabelStyle,
+                        fontSize: SizeConfig.fontSize),
+                    focusedBorder: fieldBorder.copyWith(
+                      borderSide: BorderSide(width: 2, color: AppColors.purple),
+                    ),
+                    counter: widget.maxLength != null
+                        ? Align(
+                            alignment: Alignment.centerRight.add(Alignment(
+                                SizeConfig.blockSizeHorizontal / 100 * 1.5, 0)),
+                            child: Container(
+                                // color:Colors.red,
+                                child: Text(
+                              "${widget.controller.text.length}/${widget.maxLength}",
+                              textDirection: TextDirection.rtl,
+                              style: AppTextStyles.span
+                                  .copyWith(fontSize: SizeConfig.fontSize),
+                            )),
+                          )
+                        : null)),
           ],
         ),
         widget.icon != null
             ? Positioned.fill(
-              child: Align(
-                alignment: Alignment.topRight,
-                child: FadeTransition(
-                  opacity: fade,
-                  child: GestureDetector(
-                    onTap: widget.iconOnTap,
-                    child: Container(
-                      // color: Color.fromRGBO(255, 50, 50, 0.2),
-                      color: Colors.transparent,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          SizedBox(
-                            // width: 48,
-                            child: Padding(
-                              padding: const EdgeInsets.all(9),
-                              child: Container(
-                                color: AppColors.darkBlue,
-                                child: widget.icon),
+                child: Align(
+                    alignment: Alignment.topRight,
+                    child: FadeTransition(
+                      opacity: fade,
+                      child: GestureDetector(
+                        onTap: widget.iconOnTap,
+                        child: Container(
+                          // color: Color.fromRGBO(255, 50, 50, 0.2),
+                          color: Colors.transparent,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              SizedBox(
+                                // width: 48,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(9),
+                                  child: Container(
+                                      color: AppColors.darkBlue,
+                                      child: widget.icon),
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                  )
-                )
-              )
+                    )))
             : Container()
       ],
     );
@@ -238,4 +237,3 @@ class _AppTextFormFieldState extends State<AppTextFormField> with TickerProvider
     super.dispose();
   }
 }
-
