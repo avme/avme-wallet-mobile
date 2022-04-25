6ed4ede4c7074cf4fbd08cdf0db5132fe8df9d54
Gabriel Vilarinho <contato@gabrielvilarinho.com>
2022-04-25 15:12:14 -0300

    Removed unwanted changes from previous commit

--- lib/app/screens/prototype/app_scaffold.dart
+++ lib/app/screens/prototype/app_scaffold.dart
@@ -12,10 +12,8 @@ import 'package:avme_wallet/app/screens/prototype/tokens.dart';
 import 'package:avme_wallet/app/screens/prototype/widgets/accounts_drawer.dart';
 import 'package:avme_wallet/app/screens/prototype/widgets/debug.dart';
 import 'package:avme_wallet/app/screens/widgets/theme.dart';
-import 'package:avme_wallet/app/test/webview.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:overlay_support/overlay_support.dart';
 import 'package:provider/provider.dart';
 import 'package:avme_wallet/app/screens/prototype/send.dart';
@@ -69,9 +67,9 @@ class _State extends State<AppScaffold>
   void initState() {
 
     appScaffoldTabController = TabController(
-      length: this.routeLabels.length,
-      vsync: this,
-      initialIndex: 1
+        length: this.routeLabels.length,
+        vsync: this,
+        initialIndex: 1
     );
 
     routeWidgets.addAll([
@@ -124,8 +122,8 @@ class _State extends State<AppScaffold>
       final color = connectionData[0] ? Colors.green : Colors.red;
       final message = connectionData[0] ? "Internet Connection restored" : "Lost internet connection";
       showSimpleNotification(
-        Text(message),
-        background: color
+          Text(message),
+          background: color
       );
     });
   }
@@ -137,13 +135,13 @@ class _State extends State<AppScaffold>
       connectionType = connectionData[1];
       if(connectionData[1] == ConnectivityResult.wifi)
         showSimpleNotification(
-          Text("Using a wifi connection"),
-          background: Colors.green
+            Text("Using a wifi connection"),
+            background: Colors.green
         );
       else if(connectionData[1] == ConnectivityResult.mobile)
         showSimpleNotification(
-          Text("Using a Mobile connection"),
-          background: Colors.orange
+            Text("Using a Mobile connection"),
+            background: Colors.orange
         );
       else if(connectionData[1] == ConnectivityResult.none)
         showSimpleNotification(
@@ -169,7 +167,7 @@ class _State extends State<AppScaffold>
       appBar: AppBar(
         automaticallyImplyLeading: false,
         iconTheme: IconThemeData(
-          color: AppColors.labelDefaultColor
+            color: AppColors.labelDefaultColor
         ),
         titleSpacing: appBarWidth,
 
@@ -192,10 +190,10 @@ class _State extends State<AppScaffold>
               },
             ),
             Center(
-              child: Image.asset(
-                'assets/avme_logo.png',
-                width: MediaQuery.of(context).padding.top + kToolbarHeight * 1 / 8,
-                fit: BoxFit.fitHeight,)
+                child: Image.asset(
+                  'assets/avme_logo.png',
+                  width: MediaQuery.of(context).padding.top + kToolbarHeight * 1 / 8,
+                  fit: BoxFit.fitHeight,)
             ),
             ///Changing Icon in the second drawer
             Builder(
@@ -234,14 +232,14 @@ class _State extends State<AppScaffold>
                 Flexible(
                   child: Padding(
                     padding: EdgeInsets.symmetric(
-                      horizontal: SizeConfig.safeBlockHorizontal * 6
+                        horizontal: SizeConfig.safeBlockHorizontal * 6
                     ),
                     child: Image.asset('assets/logo_font.png', fit: BoxFit.scaleDown),
                   ),
                 ),
                 Padding(
                   padding: EdgeInsets.only(top:SizeConfig.safeBlockVertical * 2),
-                 child: Text("{App Version}"),
+                  child: Text("{App Version}"),
                 )
               ],
             ),
@@ -272,19 +270,6 @@ class _State extends State<AppScaffold>
           )
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-
-        onPressed: (){
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => AppWebView()
-            )
-          );
-        },
-        tooltip: 'WebView',
-        child: const FaIcon(FontAwesomeIcons.globe,),
-      ),
     );
   }
 }
@@ -304,12 +289,12 @@ class AppBarButton extends StatelessWidget {
         width: MediaQuery.of(context).padding.top + kToolbarHeight * 1 / 1.5,
         height: MediaQuery.of(context).padding.top + kToolbarHeight * 1 / 2,
         child: Container(
-          child: Row(
-            mainAxisAlignment: this.mainAxisAlignment,
-            children: [
-              this.icon
-            ],
-          )
+            child: Row(
+              mainAxisAlignment: this.mainAxisAlignment,
+              children: [
+                this.icon
+              ],
+            )
         ),
       ),
     );
@@ -321,7 +306,7 @@ class AppTabBar extends StatefulWidget {
   // final Map<String, Widget> tabs;
   final List<String> routeLabels;
   final List<Widget> routeWidgets;
-  
+
   final double padding;
   final TabController appScaffoldTabController;
   const AppTabBar({
@@ -339,49 +324,49 @@ class _AppTabBarState extends State<AppTabBar> {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [
-        Padding(
-          padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: widget.padding),
-          child: Container(
-            decoration: BoxDecoration(
-              border: Border(
-                top: BorderSide(
-                    color: AppColors.purple,
-                    width: 2
-                )
-              )
-            ),
-            child: Theme(
-              data: avmeTheme.copyWith(
-                splashColor: Colors.transparent,
-                highlightColor: Colors.transparent,
-              ),
-              child: TabBar(
-                labelPadding: EdgeInsets.only(top:8),
-                controller: widget.appScaffoldTabController,
-                isScrollable: true,
-                indicator: BoxDecoration(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: widget.padding),
+            child: Container(
+              decoration: BoxDecoration(
                   border: Border(
-                    top: BorderSide(
-                      color: AppColors.purple,
-                      width: 2
-                    )
+                      top: BorderSide(
+                          color: AppColors.purple,
+                          width: 2
+                      )
                   )
+              ),
+              child: Theme(
+                data: avmeTheme.copyWith(
+                  splashColor: Colors.transparent,
+                  highlightColor: Colors.transparent,
                 ),
-                labelColor: Colors.white,
-                unselectedLabelColor: AppColors.labelDisabledColor,
-                tabs: getTabLabels(context, widget.appScaffoldTabController)
+                child: TabBar(
+                    labelPadding: EdgeInsets.only(top:8),
+                    controller: widget.appScaffoldTabController,
+                    isScrollable: true,
+                    indicator: BoxDecoration(
+                        border: Border(
+                            top: BorderSide(
+                                color: AppColors.purple,
+                                width: 2
+                            )
+                        )
+                    ),
+                    labelColor: Colors.white,
+                    unselectedLabelColor: AppColors.labelDisabledColor,
+                    tabs: getTabLabels(context, widget.appScaffoldTabController)
+                ),
               ),
             ),
           ),
-        ),
-        Expanded(
-          child: TabBarView(
-            controller: widget.appScaffoldTabController,
-            children: widget.routeWidgets
+          Expanded(
+            child: TabBarView(
+                controller: widget.appScaffoldTabController,
+                children: widget.routeWidgets
+            ),
           ),
-        ),
-      ]
+        ]
     );
   }
   List<Widget> getTabLabels(BuildContext context, TabController tabController)
@@ -423,35 +408,35 @@ class _AppTabBarState extends State<AppTabBar> {
       _labels.add(
         Tab(
           child:
-            Container(
-              width: (MediaQuery.of(context).size.width / 10 * 8.633) / 3,
-              child: Row(
-                // mainAxisAlignment: pos == 0 ? MainAxisAlignment.start : pos != widget.routeLabels.length - 1 ? MainAxisAlignment.center : MainAxisAlignment.end,
-                mainAxisAlignment: alignment,
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(
-                        6.0,
-                      ),
-                      color: pos == tabController.index ? AppColors.cardDefaultColor : null,
+          Container(
+            width: (MediaQuery.of(context).size.width / 10 * 8.633) / 3,
+            child: Row(
+              // mainAxisAlignment: pos == 0 ? MainAxisAlignment.start : pos != widget.routeLabels.length - 1 ? MainAxisAlignment.center : MainAxisAlignment.end,
+              mainAxisAlignment: alignment,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(
+                      6.0,
                     ),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
+                    color: pos == tabController.index ? AppColors.cardDefaultColor : null,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
                         horizontal: 8,
                         vertical: 4
-                      ),
-                      child: Text(
-                        "$key",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: SizeConfig.labelSize*0.5+6),),
                     ),
+                    child: Text(
+                      "$key",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: SizeConfig.labelSize*0.5+6),),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
-        );
+        ),
+      );
       pos++;
     });
     return _labels;
